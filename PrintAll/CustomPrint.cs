e947205d37f1572db825efbb4f5ba143ac572ab6
jprakashleo <jprakash.leo@gmail.com>
2022-06-10 11:16:46 +0530

    Add project files.

---
+++ PrintAll/CustomPrint.cs
@@ -0,0 +1,30 @@
+﻿namespace PrintAll
+{
+    
+    public class CustomPrint
+    {
+        private string name;
+        public void Print()
+        {
+            Console.WriteLine("print from lib");
+        }
+        public string Getname()
+        {
+            return name;
+        }
+        public void Printname()
+        {
+            Console.WriteLine($"here name is {name}");
+        }
+        public void  Print(string val)
+        {
+            Console.WriteLine($"here name is {val}");
+        }
+        private void PrintPrivate()
+        {
+            Console.WriteLine("here name private name");
+        }
+        public string Name => name;
+        public static string staticname => "static name";
+    }
+}
---
+++ reflactionDemo/Program.cs
@@ -0,0 +1,31 @@
+﻿//Reflection is a feature of .NET which helps reflect an assembly and its types.
+////We can use reflection to dynamically browse through all the types defined inside of an assembly. 
+using System.Reflection;
+
+Console.WriteLine("Hello, World!");
+
+var assembly = Assembly.LoadFrom(@"C:\Users\91774\VS\source\reflactionDemo\PrintAll\bin\Debug\net6.0\PrintAll.dll");
+
+foreach (var type in assembly.GetTypes())
+{
+    Console.WriteLine($"type is :{type.Name}");
+    //Console.WriteLine("================================");
+    foreach (var field in type.GetFields(BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.DeclaredOnly))
+    {
+        Console.WriteLine($"field is :{field.Name}");
+
+    }
+    Console.WriteLine("================================");
+    foreach (var method in type.GetMethods(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.DeclaredOnly))
+    {
+        Console.WriteLine($"method is {method.Name}");
+    }
+    Console.WriteLine("================================");
+
+    foreach (var propery in type.GetProperties(BindingFlags.Public | BindingFlags.NonPublic| BindingFlags.Instance|BindingFlags.Static))
+    {
+        Console.WriteLine($"peroperty is {propery.Name}");
+    }
+    Console.WriteLine("================================");
+
+}
